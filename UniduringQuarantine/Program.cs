5c5111b5fae43ae6c76d468a01277c5ccb748286
hannalonns <76208590+hannalonns@users.noreply.github.com>
2021-01-13 02:52:31 +0100

    Update Program.cs

--- UniduringQuarantine/Program.cs
+++ UniduringQuarantine/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace UniduringQuarantine
 {
@@ -6,23 +6,14 @@ namespace UniduringQuarantine
     {
         static void Main(string[] args)
 
-
         // Här nedanför körs programmet i en loop om man inte vinner , dör eller trycker på exit. 
         {
             Gameplay currentGame = new Gameplay();
             do
             {
                 currentGame.Start();
-
-                string userInput = Console.ReadLine();
-                if (userInput.ToLower() == "x")
-                {
-                    Environment.Exit(0);
-                }
             }
-
             while (true);
-
         }
     }
 }
